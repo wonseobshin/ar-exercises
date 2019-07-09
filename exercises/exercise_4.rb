a62db7020c498d2c66988b5405804b11e533eee2
Won Seob Shin <wonseob07@gmail.com>
2019-07-09 20:55:59 +0000

    exercise 4 and 5 done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where(womens_apparel: true)
+
+puts "mens stores"
+@mens_stores.each do |store|
+    puts store.name
+    puts store.annual_revenue
+end
+
+puts "womens stores generating less than 1,000,000 annually"
+@womens_stores.each do |store|
+    if store.annual_revenue < 1000000
+        puts store.name
+        puts store.annual_revenue
+    end
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "total annual revenue"
+puts Store.sum(:annual_revenue)
+
+puts "average annual revenue"
+puts Store.average(:annual_revenue)
+
+puts "number of stores generating above 1,000,000 annually"
+puts Store.where("annual_revenue > ?", 1000000).count
+
